601cb42a8cc71dae6312e668022514d00fb2c82d
PilarPinto <piapintoch@unal.edu.co>
2021-07-01 21:47:21 -0500

    [Feature]:Test

--- 0x02-react_intro/task_3/dashboard/src/App.test.js
+++ 0x02-react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
+import assert from 'assert';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders properly', () => {
+  shallow(<App />);
+});
+
+it('checking renders', () => {
+  const wrapper = shallow(<App />);
+
+  const header = wrapper.find('div.App-body');
+  const body = wrapper.find('div.App-body');
+  const footer = wrapper.find('div.App-footer')
+
+  assert.equal(header.exists(), true)
+  assert.equal(body.exists(), true);
+  assert.equal(footer.exists(), true);
 });
