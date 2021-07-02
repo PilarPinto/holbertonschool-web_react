4c234da8e6e9be4637eaf3d0c123f18772b97a38
PilarPinto <piapintoch@unal.edu.co>
2021-07-01 20:10:47 -0500

    [Feature]:Git ignore

---
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import holbertonLogo from './holbertonLogo.jpg';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className="App-header">
+        <img src={ holbertonLogo } className="App-logo" alt="mainLogo"/>
+        <header>
+         <h1 className="Dashboard-title">School Dashboard</h1>
+        </header>
+      </div>
+      <div className="App-body">
+        <p className="paragraph-title">Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p className="paragraph-footer">Copyright 2020 - holberton School</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
