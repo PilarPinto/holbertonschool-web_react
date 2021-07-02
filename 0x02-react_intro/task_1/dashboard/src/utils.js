d9559d94c7094035f6d537fbab9a4fed12be67bd
PilarPinto <piapintoch@unal.edu.co>
2021-07-01 20:40:46 -0500

    [Feature]:Source files

--- 0x02-react_intro/task_1/dashboard/src/utils.js
+++ 0x02-react_intro/task_1/dashboard/src/utils.js
@@ -2,12 +2,12 @@ export default function getFullYear() {
         return new Date().getFullYear();
     }
     
-    export function getFooterCopy(isIndex) {
-        let mssg = ''
-        if (isIndex) {
-            mssg = "Holberton School";
-        } else {
-            mssg = "Holberton School main dashboard"
-        }
-        return mssg;
+export function getFooterCopy(isIndex) {
+    let mssg = ''
+    if (isIndex) {
+        mssg = "Holberton School";
+    } else {
+        mssg = "Holberton School main dashboard"
     }
+    return mssg;
+}
