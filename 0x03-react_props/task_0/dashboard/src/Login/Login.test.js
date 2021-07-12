9df2f6645a707ad88460b197b30d01838f74585c
PilarPinto <piapintoch@unal.edu.co>
2021-07-12 12:06:10 -0500

    [Feature]:Project dscription

--- 0x03-react_props/task_0/dashboard/src/Login/Login.test.js
+++ 0x03-react_props/task_0/dashboard/src/Login/Login.test.js
@@ -12,18 +12,5 @@ describe("Testing the behavior of App component", () => {
 		            wrapper.update();
 		            expect(wrapper.children()).toHaveLength(6);
 		        });
-})import React from "react";
-import Login from "./Login";
-import { shallow } from 'enzyme';
-
-describe("Testing the behavior of App component", () => {
-	    it("App renders without crashing", () => {
-		            const wrapper = shallow(<Login />);
-		            expect(wrapper.exists()).toEqual(true);
-		        });
-	    it("App renders App-body", () => {
-		            const wrapper = shallow(<Login />);
-		            wrapper.update();
-		            expect(wrapper.children()).toHaveLength(6);
-		        });
 });
+
